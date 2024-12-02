193fc40c1f33d84cf45ceae13e90d6fa69b49920
Parul-Kudtarkar <parulbioinf@gmail.com>
2024-12-02 17:33:24 +0000

    change db

--- cdk/infrastructure/constants.py
+++ cdk/infrastructure/constants.py
@@ -1,2 +1,2 @@
 DEV_DATABASE_IDENTIFIER = 'igvfd-dev-deploydevelopme-postgrespostgresfromsnap-id7ckmmefopt'
-PROD_DATABASE_IDENTIFIER = 'igvfd-main-stagingdeploys-postgrespostgresfromsnap-u3un6qydxand'
+PROD_DATABASE_IDENTIFIER = 'igvfd-main-productiondepl-postgrespostgresfromsnap-eqhlg3wgbk36'
